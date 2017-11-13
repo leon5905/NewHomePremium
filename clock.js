bc41583ad9a12f8defa3915d13ba44a293b45c64
°﹏⊙★╮小囚 <khanghng6268@gmail.com>
2017-11-13 17:37:23 +0800

    add second clock

--- clock.js
+++ clock.js
@@ -1,15 +1,24 @@
-$.getScript("clock/clock-1.1.0.min.js");
-
-var clock = $(".clock").clock({
-    width: 300,       // set width
-    height: 450,      // set height
-    theme: 't2',      // set theme  => 't1' 't2' 't3'
-    date: new Date()  // set date => new Date()
-})
-var clock = $("#clock").clock(),
-data = clock.data();
-// start the clock
-data.start();
-// set a new date
-data.setTime(new Date());
-
+var clock={
+    oriClock:function(){
+        $.getScript("clock/clock-1.1.0.min.js");
+        
+        var clock = $(".clock").clock({
+            width: 300,       // set width
+            height: 450,      // set height
+            theme: 't2',      // set theme  => 't1' 't2' 't3'
+            date: new Date()  // set date => new Date()
+        })
+        var clock = $("#clock").clock(),
+        data = clock.data();
+        // start the clock
+        data.start(); 
+        // set a new date
+        data.setTime(new Date());
+    },
+    clock1:function(){
+        var clock = new FlipClock($('.clock'), 100, {
+            
+                clockFace: 'TwentyFourHourClock'
+            });
+    }
+}
